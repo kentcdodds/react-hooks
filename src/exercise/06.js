ff90a101b974cc2b44b1dc4c62e5e3d63f3e150d
Arunothia <arunothiamarappan@gmail.com>
2024-03-04 17:56:20 -0700

    Completed exercise 6

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,32 +1,26 @@
+/* eslint-disable react/prop-types */
 // useEffect: HTTP requests
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
+import {fetchPokemon} from '../pokemon'
+import {PokemonInfoFallback} from '../pokemon'
+import {PokemonDataView} from '../pokemon'
 import {PokemonForm} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
-  // 🐨 Have state for the pokemon (null)
-  // 🐨 use React.useEffect where the callback should be called whenever the
-  // pokemon name changes.
-  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
-  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null.
-  // (This is to enable the loading state when switching between different pokemon.)
-  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => {/* update all the state here */},
-  //   )
-  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
-  //   1. no pokemonName: 'Submit a pokemon'
-  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
+  const [pokemonState, setPokemonState] = React.useState(null)
+  React.useEffect(() => {
+    if (!pokemonName) return
+    setPokemonState(null)
+    fetchPokemon(pokemonName).then(pokemonData => {
+      setPokemonState(pokemonData)
+    })
+  }, [pokemonName])
 
-  // 💣 remove this
-  return 'TODO'
+  if (!pokemonName) return 'Submit a pokemon'
+  if (!pokemonState) return <PokemonInfoFallback name={pokemonName} />
+  return <PokemonDataView pokemon={pokemonState} />
 }
 
 function App() {
